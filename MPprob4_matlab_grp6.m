63eee9a1cdd93d2e99033544df3e7dad98af0829
jalabaz <57590283+jalabaz@users.noreply.github.com>
2019-12-08 14:02:47 +0800

    Update MPprob4_matlab_grp6.m

--- MPprob4_matlab_grp6.m
+++ MPprob4_matlab_grp6.m
@@ -3,7 +3,9 @@ vo = input('Magnitude of velocity (m/s): ');
 theta = input('Angle (degrees) at which the projectile is fired: ');
 ax = input('Acceleration (m/s^2) in the x-component(consider the sign!): ');
 ay = input('Acceleration (m/s^2) in the y-component(consider the sign!): ');
-if ay == 0
+if yo < 0
+    error('Height should be at or above ground!');
+elseif ay >= 0
     error('Zero acceleration is impossible for free fall!');
 end
 t = (vo*sind(theta)+sqrt(((vo*sind(theta))^2)-2*(ay)*yo))/(-ay);    %time
